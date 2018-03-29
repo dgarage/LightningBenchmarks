bab44c2ab22b7572411e1cd3ed2d6fd1d3fdd4bc
nicolas.dorier <nicolas.dorier@gmail.com>
2018-03-29 15:12:12 +0900

    Initial commit

---
+++ src/Lightning.Alice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lightning.Alice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
